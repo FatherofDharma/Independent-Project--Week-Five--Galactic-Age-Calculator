431f24f9c4185cc120c36d5925c5fae284253c55
FatherofDharma <fatherofdharma@gmail.com>
2019-06-28 11:46:33 -0700

    change tests to reflect refactor of convertAge function.

--- spec/galacticCalculatorspec.js
+++ spec/galacticCalculatorspec.js
@@ -6,15 +6,18 @@ describe('Human', function() {
     expect(earthling.name).toEqual('Daniel');
     expect(earthling.age).toEqual(36);
   });
+//this test is no longer valid as written as the method now stores new values rather than returning them. This is why it is now commented out.
 
-  it('should test if the function convertAge can return the Humans age in mercury years', function() {
-    let earthling = new Human('Daniel', 36)
-    expect(earthling.convertAge()).toEqual(8.64);
-  })
+  // it('should test if the function convertAge can return the Humans age in mercury years', function() {
+  //   let earthling = new Human('Daniel', 36)
+  //   expect(earthling.convertAge()).toEqual(8.64);
+  // })
+
+//testthat redefined new behaviors for the convertAge function by storing the value instead of returning it.
   it('should test if the function convertAge can store the new value in the Human object', function() {
     let earthling = new Human ('Daniel', 36)
-    expect(earthling.convertAge()).toEqual(8.64);
-    console.log(earthling.mercAge)
+    earthling.convertAge();
     expect(earthling.mercAge).toEqual(8.64);
   })
+
 });
