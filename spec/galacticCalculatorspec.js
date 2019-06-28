807e996ec142810424da2505be1f7e2ea20e5b8d
FatherofDharma <fatherofdharma@gmail.com>
2019-06-28 10:48:30 -0700

    add function to convert age to mercury age, and pass test.

--- spec/galacticCalculatorspec.js
+++ spec/galacticCalculatorspec.js
@@ -3,14 +3,12 @@ describe('Human', function() {
 
   it('should test that name and age are stored in class Human', function() {
     let earthling = new Human('Daniel', 36)
-    console.log(earthling);
     expect(earthling.name).toEqual('Daniel');
     expect(earthling.age).toEqual(36);
   });
 
-  it('should test if the function convertMercury can return the Human's age in mercury years', function() {
+  it('should test if the function convertMercury can return the Humans age in mercury years', function() {
     let earthling = new Human('Daniel', 36)
-    console.log(earthling.age)
-    expect(earthling.convertMercury).toEqual()
+    expect(earthling.convertMercury()).toEqual(8.64);
   })
 });
--- src/galacticAge.js
+++ src/galacticAge.js
@@ -6,5 +6,11 @@ export class Human {
   constructor(name, age) {
     this.name = name;
     this.age = age;
+    this.mercAge = 0;
+  }
+
+  convertMercury() {
+    console.log('I am in the function');
+    return (this.age * .24);
   }
 }
